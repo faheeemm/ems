08871f2d31a53a5c03e495428c25e613d042e8d3
faheeemm <faheemahmedhyd82@gmail.com>
2026-05-20 01:25:04 +0530

    implemented useState features in Login page for email and password

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,17 +1,54 @@
-import React from "react"
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("email is:", email);
+    console.log("password is:", password);
+
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <div className="border-2 rounded-xl border-emerald-600 p-20">
-        <form className="flex flex-col items-center justify-center gap-2">
-          <input className="border-2 border-emerald-600 text-xl outline-none bg-transparent py-3 px-5 rounded-full placeholder:text-gray-400" type="email" placeholder="e.g., name@example.com" />
-          <input className="border-2 border-emerald-600 text-xl outline-none bg-transparent py-3 px-5 rounded-full placeholder:text-gray-400" type="password" placeholder="Enter password" />
-          <button className="text-white outline-none border-none bg-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400">Log in</button>
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+          className="flex flex-col items-center justify-center"
+        >
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+            className="border-2 border-emerald-600 outline-none font-medium text-lg bg-transparent py-2 px-6 rounded-full placeholder:text-gray-400"
+            type="email"
+            placeholder="name@example.com"
+          />
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+            className="border-2 border-emerald-600 outline-none font-medium text-lg bg-transparent py-2 px-6 rounded-full mt-3 placeholder:text-gray-400"
+            type="password"
+            placeholder="Enter password"
+          />
+          <button className="mt-7 text-white border-none hover:bg-emerald-700 font-semibold bg-emerald-600 text-lg py-2 px-8 w-full rounded-full placeholder:text-white">
+            Log in
+          </button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
