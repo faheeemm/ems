11e5bcaabdacaed7d2c7e625d0d92edbdae9533a
faheeemm <faheemahmedhyd82@gmail.com>
2026-05-26 04:23:19 +0530

    optimized and readable code in CompleteTask

--- src/components/TaskList/CompleteTask.jsx
+++ src/components/TaskList/CompleteTask.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const CompleteTask = ({data}) => {
+const CompleteTask = ({ data }) => {
   return (
     <div className="flex-shrink-0 h-full w-[400px] p-5 bg-blue-400 rounded-xl">
       <div className="flex items-center justify-between">
