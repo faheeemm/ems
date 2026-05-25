93cfd81687e95f2074107d522e7ced1357aaf057
faheeemm <faheemahmedhyd82@gmail.com>
2026-05-26 03:25:55 +0530

    passing data in tasks

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 
-const AcceptTask = () => {
+const AcceptTask = ({ data }) => {
   return (
     <div className="flex-shrink-0 h-full w-[400px] p-5 bg-red-400 rounded-xl">
       <div className="flex items-center justify-between">
-        <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
-        <h4 className="text-sm">20 May 2026</h4>
+        <h3 className="bg-red-600 text-sm px-3 py-1 rounded">{data.category}</h3>
+        <h4 className="text-sm">{data.taskDate}</h4>
       </div>
-      <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
-      <p className="text-sm mt-2">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book
-      </p>
+      <h2 className="mt-5 text-2xl font-semibold">{data.taskTitle}</h2>
+      <p className="text-sm mt-2">{data.taskDescription}</p>
       <div className="flex justify-between mt-4">
-        <button className="bg-green-500 py-1 px-2 text-sm">Mark as Completed</button>
+        <button className="bg-green-500 py-1 px-2 text-sm">
+          Mark as Completed
+        </button>
         <button className="bg-red-500 py-1 px-2 text-sm">Mark as Failed</button>
       </div>
     </div>
--- src/components/TaskList/FailedTask.jsx
+++ src/components/TaskList/FailedTask.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 
-const FailedTask = () => {
+const FailedTask = ({data}) => {
   return (
     <div className="flex-shrink-0 h-full w-[400px] p-5 bg-yellow-400 rounded-xl">
       <div className="flex items-center justify-between">
-        <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
-        <h4 className="text-sm">20 May 2026</h4>
+        <h3 className="bg-red-600 text-sm px-3 py-1 rounded">
+          {data.category}
+        </h3>
+        <h4 className="text-sm">{data.taskDate}</h4>
       </div>
-      <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
-      <p className="text-sm mt-2">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book
-      </p>
+      <h2 className="mt-5 text-2xl font-semibold">{data.taskTitle}</h2>
+      <p className="text-sm mt-2">{data.taskDescription}</p>
       <div className="mt-2">
-        <button className="w-full">Failed</button>
+        <button className="w-full bg-red-500 py-1 px-2 text-sm">Failed</button>
       </div>
     </div>
   );
