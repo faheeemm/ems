88bf649aaf3d1232333d5d962d32da86d18da940
faheeemm <faheemahmedhyd82@gmail.com>
2026-05-26 04:22:43 +0530

    optimized and readable code in AcceptTask

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -4,7 +4,9 @@ const AcceptTask = ({ data }) => {
   return (
     <div className="flex-shrink-0 h-full w-[400px] p-5 bg-red-400 rounded-xl">
       <div className="flex items-center justify-between">
-        <h3 className="bg-red-600 text-sm px-3 py-1 rounded">{data.category}</h3>
+        <h3 className="bg-red-600 text-sm px-3 py-1 rounded">
+          {data.category}
+        </h3>
         <h4 className="text-sm">{data.taskDate}</h4>
       </div>
       <h2 className="mt-5 text-2xl font-semibold">{data.taskTitle}</h2>
