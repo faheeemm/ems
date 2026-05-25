a9e3a12ea6e4f202d31a64b6eb2f1a0da8ab02e5
faheeemm <faheemahmedhyd82@gmail.com>
2026-05-26 04:24:14 +0530

    optimized and readable code in TaskList

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -6,22 +6,23 @@ import FailedTask from "./FailedTask";
 
 const TaskList = ({ data }) => {
   return (
-    <div id="tasklist" className="h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10">
+    <div
+      id="tasklist"
+      className="h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10"
+    >
       {data.tasks.map((elem, idx) => {
-
         if (elem.active) {
-          return <AcceptTask key={idx} data={elem} />
+          return <AcceptTask key={idx} data={elem} />;
         }
         if (elem.newTask) {
-          return <NewTask key={idx} data={elem} />
+          return <NewTask key={idx} data={elem} />;
         }
         if (elem.completed) {
-          return <CompleteTask key={idx} data={elem} />
+          return <CompleteTask key={idx} data={elem} />;
         }
         if (elem.failed) {
-          return <FailedTask key={idx} data={elem} />
+          return <FailedTask key={idx} data={elem} />;
         }
-          
       })}
     </div>
   );
