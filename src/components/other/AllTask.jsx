46a8b5b0b9d790c15893e163546451201c0d24d5
faheeemm <faheemahmedhyd82@gmail.com>
2026-05-26 04:19:32 +0530

    passing data in admin tasks table

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -10,21 +10,21 @@ const AllTask = () => {
       className="bg-[#1C1C1C] p-5 rounded mt-5"
     >
       <div className="bg-red-400 mb-2 py-2 px-4 flex justify-between rounded">
-        <h2 className="h-1/5 font-semibold">Employee Name</h2>
-        <h3 className="w-1/5 font-semibold">New Task</h3>
-        <h5 className="w-1/5 font-semibold">Active Task</h5>
-        <h5 className="w-1/5 font-semibold">Completed</h5>
-        <h5 className="w-1/5 font-semibold">Failed</h5>
+        <h2 className="text-lg font-medium w-1/5">Employee Name</h2>
+        <h3 className="text-lg font-medium w-1/5">New Task</h3>
+        <h5 className="text-lg font-medium w-1/5">Active Task</h5>
+        <h5 className="text-lg font-medium w-1/5">Completed</h5>
+        <h5 className="text-lg font-medium w-1/5">Failed</h5>
       </div>
       <div className="child overflow-auto">
         {authData.employees.map((elem) => {
           return (
             <div className="border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded">
-              <h2 className="h-1/5">{elem.firstName}</h2>
-              <h3 className="w-1/5 text-white pl-14">Task</h3>
-              <h5 className="w-1/5 text-white pl-9">Status</h5>
-              <h5 className="w-1/5 text-white pl-4">Status</h5>
-              <h5 className="w-1/5 text-white">Status</h5>
+              <h2 className=" text-lg font-medium w-1/5">{elem.firstName}</h2>
+              <h3 className=" text-lg font-medium w-1/5 text-white">{elem.taskNumbers.newTask}</h3>
+              <h5 className=" text-lg font-medium w-1/5 text-white">{elem.taskNumbers.active}</h5>
+              <h5 className=" text-lg font-medium w-1/5 text-white">{elem.taskNumbers.completed}</h5>
+              <h5 className=" text-lg font-medium w-1/5 text-white">{elem.taskNumbers.failed}</h5>
             </div>
           );
         })}
