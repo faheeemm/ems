d5afb7a6b5f14c886878d196d738ad59986a85da
faheeemm <faheemahmedhyd82@gmail.com>
2026-05-26 02:09:35 +0530

    setting up who is logged in localStorage part 2

---
+++ src/components/TaskList/AcceptTask.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const AcceptedTask = () => {
+  return (
+    <div className="flex-shrink-0 h-full w-[400px] p-5 bg-red-400 rounded-xl">
+      <div className="flex items-center justify-between">
+        <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+        <h4 className="text-sm">20 May 2026</h4>
+      </div>
+      <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+      <p className="text-sm mt-2">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book
+      </p>
+      <div className="flex justify-between mt-4">
+        <button className="bg-green-500 py-1 px-2 text-sm">Mark as Completed</button>
+        <button className="bg-red-500 py-1 px-2 text-sm">Mark as Failed</button>
+      </div>
+    </div>
+  );
+};
+
+export default AcceptedTask;
